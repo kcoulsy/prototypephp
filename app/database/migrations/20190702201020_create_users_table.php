08e2cbb4ddf341adbb84ccd654afe3429bff9575
Kristian <kristian@coulsy.co.uk>
2019-07-02 21:21:11 +0100

    Added initial migration for users table

---
+++ app/database/migrations/20190702201020_create_users_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersTable extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('username', 'string', ['limit' => 255])
+                ->addColumn('email', 'string', ['limit' => 255])
+                ->addColumn('password', 'string', ['limit' => 255])
+                ->addColumn('created_at', 'datetime')
+                ->addColumn('updated_at', 'datetime', ['null' => true])
+                ->create();
+    }
+}
---
+++ app/phinx.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'init.php';
+
+return  [
+    'paths' => [
+        'migrations' => 'database/migrations'
+    ],
+    'environments' => [
+        'default_migration_table' => 'migrations',
+        'default' => [
+            'adapter' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'port' => getenv('DB_PORT'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'name' => getenv('DB_DATABASE')
+        ]
+    ]
+];
