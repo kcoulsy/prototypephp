8b0932df00e0281847009126181a92fd44378cf2
Kristian <kristian@coulsy.co.uk>
2019-07-02 17:42:17 +0100

    Refactored how the url parses routes

--- app/core/App.php
+++ app/core/App.php
@@ -39,14 +39,18 @@ class App
     }
 
     /**
-     * Returns the parsed url as an array.
+     * Returns the parsed request as an array.
      *
-     * @return array url
+     * @return array request
      */
-    public function parseUrl()
+    public function parseRequest()
     {
         if (isset($_GET['url'])) {
-            return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+            $params = $_GET;
+
+            $params['url'] = explode('/', filter_var(rtrim($params['url'], '/'), FILTER_SANITIZE_URL));
+
+            return $params;
         }
     }
 
@@ -55,36 +59,75 @@ class App
      */
     public function updateMethod()
     {
-        $url = $this->parseUrl();
+        $req = $this->parseRequest();
+        $method = $this->method;
 
-        // Setting the controller
-        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
-            $this->controller = $url[0];
-            unset($url[0]);
+        // splitting out url from req
+        $url = $req['url'];
+        unset($req['url']);
+
+        if (isset($url)) {
+            if (count($url) > 1) {
+                $controller = join('/', $url);
+
+                // check if the whole string is the controller
+                // ie, we are calling the index() method
+                if (!$this->checkAndSetController($controller)) {
+                    // pop off the last value and use it as a method
+                    $method = end($url);
+                    array_pop($url);
+
+                    // rejoin and check for the controller
+                    $controller = join('/', $url);
+
+                    //check the new controller
+                    $this->checkAndSetController($controller);
+                }
+
+            } else {
+                // checking if the url string is a controller, if it is then use it,
+                // else it falls back to using it as a method on Home
+                $url_string = $url[0];
+                if (!$this->checkAndSetController($url_string)) {
+                    $method = $url_string;
+                }
+            }
         }
 
         require_once '../app/controllers/' . $this->controller . '.php';
 
         $this->controller = new $this->controller;
 
-        // setting the method
-        if (isset($url[1])) {
-            if (method_exists($this->controller, $url[1])) {
-                $this->method = $url[1];
-                unset($url[1]);
+        // checking that the set method exists, if it does then use it
+        if (isset($method)) {
+            if (method_exists($this->controller, $method)) {
+                $this->method = $method;
             }
         }
 
         $this->type = $_SERVER['REQUEST_METHOD'];
 
-        // setting the params and call the controller/method
+        // setting the params nased on method type
         if ($this->type == 'POST') {
             $this->params = $_POST;
         } else {
-            $this->params = $url ? array_values($url) : [];
+            $this->params = $req;
         }
 
         $this->callMethod();
+    }
+
+    /**
+     * Checking the the controller exists, if it does then set it.
+     *
+     * @return bool
+     */
+    private function checkAndSetController($controller) {
+        if (file_exists('../app/controllers/' . $controller . '.php')) {
+            $this->controller = $controller;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -92,10 +135,6 @@ class App
      */
     public function callMethod()
     {
-        if ($this->type == 'POST') {
-            call_user_func([$this->controller, $this->method], $this->params);
-        } else {
-            call_user_func_array([$this->controller, $this->method], $this->params);
-        }
+        call_user_func([$this->controller, $this->method], $this->params);
     }
 }
