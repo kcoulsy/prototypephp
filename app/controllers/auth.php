c09289d43ab782eb4c9cf3b76618ed6fa12db90f
Kristian <kristian@coulsy.co.uk>
2019-07-03 18:51:47 +0100

    #auth - refactored register into AuthController

--- app/controllers/auth.php
+++ app/controllers/auth.php
@@ -23,81 +23,22 @@ class Auth extends Controller
      */
     public function register($params)
     {
-        $username = $this->validateUsername($params);
-        $email = $this->validateEmail($params);
-        $password = $this->validatePassword($params);
-
-        $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-
-        User::create([
-            'username' => $username,
-            'email' => $email,
-            'password' => $password_hashed
-        ]);
-
-        $this->redirect('/');
-    }
-
-    private function validateUsername($params)
-    {
-        if (!isset($params['username'])) {
-            throw new Exception('Please enter a username');
+        try {
+            $this->auth_controller->register($params);
+        } catch(Exception $e) {
+            $this->view('home/register.html', [
+                'error' => $e->getMessage()
+            ]);
         }
-
-        $username = trim($params['username']);
-
-        $user = User::where('username', $username);
-
-        if ($user->count() > 0) {
-            throw new Exception('Username Taken');
-        }
-
-        return $username;
-    }
-
-    private function validateEmail($params)
-    {
-        if (!isset($params['email'])) {
-            throw new Exception('Please enter an Email');
-        }
-
-        $email = trim($params['email']);
-
-        $user = User::where('email', $email);
-
-        if ($user->count() > 0) {
-            throw new Exception('Email Taken');
-        }
-
-        return $email;
-    }
-
-    private function validatePassword($params)
-    {
-        if (!isset($params['password'])) {
-            throw new Exception('Please enter an Password');
-        }
-
-        if (!isset($params['confirm'])) {
-            throw new Exception('Please confirm your Password');
-        }
-
-        if ($params['password'] !== $params['confirm']) {
-            throw new Exception('Your passwords must match!');
-        }
-
-        return $params['password'];
     }
 
     public function login($params)
     {
         try {
-
             $this->auth_controller->login(
                 $params['username'],
                 $params['password']
             );
-
         } catch(Exception $e) {
             $this->view('home/login.html', [
                 'error' => $e->getMessage()
--- app/helpers/AuthController.php
+++ app/helpers/AuthController.php
@@ -2,10 +2,66 @@
 
 class AuthController extends Controller {
 
+    public function register($params)
+    {
+        try {
+            if (!isset($params['username'])) {
+                throw new Exception('Please enter a username');
+            }
+
+            $username = trim($params['username']);
+
+            $user = User::where('username', $username);
+
+            if ($user->count() > 0) {
+                throw new Exception('Username Taken');
+            }
+
+            if (!isset($params['email'])) {
+                throw new Exception('Please enter an Email');
+            }
+
+            $email = trim($params['email']);
+
+            $user = User::where('email', $email);
+
+            if ($user->count() > 0) {
+                throw new Exception('Email Taken');
+            }
+
+            if (!isset($params['password'])) {
+                throw new Exception('Please enter an Password');
+            }
+
+            if (!isset($params['confirm'])) {
+                throw new Exception('Please confirm your Password');
+            }
+
+            if ($params['password'] !== $params['confirm']) {
+                throw new Exception('Your passwords must match!');
+            }
+
+            $password = $params['password'];
+
+            $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+
+            User::create([
+                'username' => $username,
+                'email' => $email,
+                'password' => $password_hashed
+            ]);
+
+            $this->redirect('/');
+
+        } catch(Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
     public function login($username, $password)
     {
         if (!isset($username)) {
-            throw new Exception('Please enter a username');
+            throw new Exception('Please enter a Username');
         }
 
         if (!isset($password)) {
