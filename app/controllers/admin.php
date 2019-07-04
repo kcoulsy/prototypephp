f365f4c6dc6d9a066b65422fef589007d0df7e64
Kristian <kristian@coulsy.co.uk>
2019-07-04 19:27:23 +0100

    #admin - added users page with pagination

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -7,7 +7,24 @@ class Admin extends Controller
      */
     public function index()
     {
-
         $this->view('admin/index.html');
+    }
+
+    /**
+     * Users page of admin panel
+     */
+    public function users($params)
+    {
+        if (isset($params['page'])) {
+            $currentPage = $params['page'];
+        } else {
+            $currentPage = 1;
+        }
+
+        $users = User::paginate(2, ['*'], 'page', $currentPage)->withPath('/admin/users')->toArray();
+
+        $users['current_page'] = $currentPage;
+
+        $this->view('admin/users.html', $users);
     }
 }
