772fcd1cca49369a7c47ebcee34dec5813bb036f
Kristian <kristian@coulsy.co.uk>
2019-07-09 21:03:44 +0100

    #profile - initial profile commit, uploads user profile image

--- app/controllers/profile.php
+++ app/controllers/profile.php
@@ -19,7 +19,30 @@ class Profile extends Controller
             'user_id'
         ]);
 
-        echo 'hereo';
-        die();
+        $user_id = $params['user_id'];
+
+        try {
+            $current_user = $this->getUser();
+            if ($current_user->id != $user_id) {
+
+                $can_update = AuthController::hasRole('admin.profile.update');
+                if (!$can_update) {
+                    throw new Exception('You do not have the required role to update another user profile!');
+                }
+            }
+
+            $user = User::find($params['user_id']);
+
+            if ($_FILES['profile_image']['size'] > 0) {
+                $uc = new UploadController(['jpg', 'png', 'jpeg', 'gif']);
+                $new_profile_image = $uc->upload(null, 'profile_image');
+                $user->userExtended()->update(['profile_image' => '/' . $new_profile_image]);
+            }
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            die();
+        }
+
+        $this->redirect('/admin/profile?id=' . $params['user_id']);
     }
 }
